d4554444baecc11c4133448864ed26f39635dbb9
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-06-24 10:42:18 +0900

    add fake modules

---
+++ tests/FakeNowModuleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ray\IdentityValueModule;
+
+use PHPUnit\Framework\TestCase;
+use Ray\Di\Injector;
+
+class FakeNowModuleTest extends TestCase
+{
+    public function testFakeNowModule()
+    {
+        $now = (new Injector(new FakeIdentityValueModule(new FakeNowModule)))->getInstance(NowInterface::class);
+        $this->assertInstanceOf(FakeNow::class, $now);
+    }
+}
---
+++ tests/FakeUuidModuleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ray\IdentityValueModule;
+
+use PHPUnit\Framework\TestCase;
+use Ray\Di\Injector;
+
+class FakeUuidModuleTest extends TestCase
+{
+    public function testFakeUUidModule()
+    {
+        $uuid = (new Injector(new FakeIdentityValueModule(new FakeUuidModule)))->getInstance(UuidInterface::class);
+        $this->assertInstanceOf(FakeUuid::class, $uuid);
+    }
+}
