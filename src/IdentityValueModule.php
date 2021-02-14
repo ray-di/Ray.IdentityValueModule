92b80cff82b674ed11b75f09252297e51b622e6b
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-02-14 11:56:43 +0900

    Bind DateTimeInterface

--- src/IdentityValueModule.php
+++ src/IdentityValueModule.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Ray\IdentityValueModule;
 
+use DateTimeImmutable;
+use DateTimeInterface;
 use Ray\Di\AbstractModule;
 
 class IdentityValueModule extends AbstractModule
@@ -17,5 +19,6 @@ class IdentityValueModule extends AbstractModule
         $this->bind(NowInterface::class)->to(Now::class);
         $this->bind(DbDateTimeInterface::class)->to(DbDateTime::class);
         $this->bind(Iso8601Interface::class)->to(Iso8601::class);
+        $this->bind(DateTimeInterface::class)->to(DateTimeImmutable::class);
     }
 }
--- tests/FakeNowModuleTest.php
+++ tests/FakeNowModuleTest.php
@@ -4,14 +4,35 @@ declare(strict_types=1);
 
 namespace Ray\IdentityValueModule;
 
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
 use PHPUnit\Framework\TestCase;
 use Ray\Di\Injector;
 
+use function assert;
+
 class FakeNowModuleTest extends TestCase
 {
+    /** @var Injector */
+    private $injector;
+
+    protected function setUp(): void
+    {
+        $this->injector = new Injector(new FakeNowModule());
+    }
+
     public function testFakeNowModule(): void
     {
-        $now = (new Injector(new FakeIdentityValueModule(new FakeNowModule())))->getInstance(NowInterface::class);
+        $now = $this->injector->getInstance(NowInterface::class);
         $this->assertInstanceOf(FakeNow::class, $now);
+    }
+
+    public function testDateTimeInterface(): void
+    {
+        $dateTime = $this->injector->getInstance(DateTimeInterface::class);
+        $this->assertInstanceOf(DateTimeImmutable::class, $dateTime);
+        assert($dateTime instanceof DateTimeImmutable);
+        $this->assertSame('1970-01-01T00:00:00+00:00', $dateTime->format(DateTime::ATOM));
     }
 }
